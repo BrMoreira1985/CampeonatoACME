a1ee63ff0a106bf3a90eece23e2c075f37726634
Bruno Moreira <ok.computador@gmail.com>
2022-06-27 18:44:15 -0300

    Style tweaks

--- index.js
+++ index.js
@@ -1,101 +1,96 @@
 const request = new XMLHttpRequest();
-request.open("GET","./data.json", false);
+request.open("GET", "./data.json", false);
 request.send(null);
 const data = JSON.parse(request.responseText);
 
-let table = document.createElement('table');
-let thead = document.createElement('thead');
-let tbody = document.createElement('tbody');
-let tfoot = document.createElement('tfoot');
-let row =[];
+let table = document.createElement("table");
+let thead = document.createElement("thead");
+let tbody = document.createElement("tbody");
+let tfoot = document.createElement("tfoot");
+let row = [];
 let rowData = [];
 
 table.appendChild(thead);
 table.appendChild(tbody);
 table.appendChild(tfoot);
 
-function stripStatistics(statistics){
-    statistics = statistics.split(/\s/);
-    return statistics;
+function stripStatistics(statistics) {
+  statistics = statistics.split(/\s/);
+  return statistics;
 }
 
-function buildHeader(){
-    let empty1 = document.createElement('th');
-    let empty2 = document.createElement('th');
-    thead.appendChild(empty1);
-    thead.appendChild(empty2);
+function buildHeader() {
+  let empty1 = document.createElement("th");
+  let empty2 = document.createElement("th");
+  thead.appendChild(empty1);
+  thead.appendChild(empty2);
 
-    let length = data.data[20].headers.length;
+  let length = data.data[20].headers.length;
 
-    for(let k = 0; k < length; k++){
-        let hdr = data.data[20].headers[k]
-        let thHdr = document.createElement('th');
-        thHdr.innerHTML = hdr;
-        thead.appendChild(thHdr)
-    } 
+  for (let k = 0; k < length; k++) {
+    let hdr = data.data[20].headers[k];
+    let thHdr = document.createElement("th");
+    thHdr.innerHTML = hdr;
+    thead.appendChild(thHdr);
+  }
 }
 
-function buildFoot(){
-    let line1Foot = document.createElement('th');
-    let line2Foot = document.createElement('th');
-    let label = [];
-    let labelLength = data.data[21].footPositions.length;
-    let positions = data.data[21];
+function buildFoot() {
+  let line1Foot = document.createElement("th");
+  let line2Foot = document.createElement("th");
+  let label = document.createElement("td");
+  let labelLength = data.data[21].footPositions.length;
+  let positions = data.data[21];
+  let labelText = "";
 
-    for(let l = 0; l < labelLength; l++){
-        
-        label[l] = document.createElement('td');
-        label[l].innerHTML =`&emsp; <i class="fa-solid fa-square"></i>  ${positions.footPositions[l]}` ;
-        line1Foot.append(label[l]);
+  for (let l = 0; l < labelLength; l++)
+    labelText += `&emsp; <i class="fa-solid fa-square lbl${l}"></i>  ${positions.footPositions[l]}`;
 
+  label.innerHTML = labelText;
+  line1Foot.append(label);
+  label = document.createElement("td");
+  labelLength = data.data[22].footAcronyms.length;
+  let acronyms = data.data[22];
+  labelText = "";
+
+  for (let m = 0; m < labelLength; m++)
+    labelText += `&emsp;${acronyms.footAcronyms[m]}`;
+
+  label.innerHTML = labelText;
+  line2Foot.append(label);
+
+  tfoot.append(line1Foot);
+  tfoot.append(line2Foot);
+}
+
+function buildTable() {
+  buildHeader();
+  buildFoot();
+  for (let i = 0; i < 20; i++) {
+    let index = document.createElement("td");
+    let name = document.createElement("td");
+    let stats = stripStatistics(data.data[i].statistics);
+
+    row[i] = document.createElement("tr");
+    tbody.appendChild(row[i]);
+
+    index.innerHTML = i + 1;
+    row[i].appendChild(index);
+
+    name.innerHTML = data.data[i].name;
+    row[i].appendChild(name);
+
+    length = stats.length;
+
+    for (let j = 0; j < length; j++) {
+      let _data = stats.shift();
+      rowData[j] = document.createElement("td");
+      rowData[j].innerHTML = _data;
+      row[i].appendChild(rowData[j]);
     }
+  }
 
-    label = document.createElement('td');
-    labelLength = data.data[22].footAcronyms.length;
-    let acronyms = data.data[22];
-    let labelText = '';
-
-    for(let m = 0; m < labelLength; m++) 
-        labelText += `&emsp;${acronyms.footAcronyms[m]}`;
-    
-    label.innerHTML = labelText
-    line2Foot.append(label);
-
-
-    tfoot.append(line1Foot);
-    tfoot.append(line2Foot);
-
-
-}
-
-function buildTable(){
-    buildHeader();
-    buildFoot();
-    for(let i = 0; i < 20; i++){
-        let index = document.createElement('td');
-        let name = document.createElement('td');
-        let stats = stripStatistics(data.data[i].statistics);
-
-        row[i] = document.createElement('tr');
-        tbody.appendChild(row[i]);
-        
-        index.innerHTML = i+1;
-        row[i].appendChild(index);
-        
-        name.innerHTML = data.data[i].name;
-        row[i].appendChild(name);
-        
-        length = stats.length;
-
-        for(let j = 0; j < length; j++){
-            let _data = stats.shift();
-            rowData[j] = document.createElement('td');
-            rowData[j].innerHTML = _data;
-            row[i].appendChild(rowData[j]);
-        }
-    }
-
-    document.getElementById('body').appendChild(table);
+  document.getElementById("body").appendChild(table);
 }
 
 buildTable();
